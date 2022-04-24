a91f19727ce9c8e2b1b9265ba22ff2c79e8b8eb0
Diego Linhares <diegomlinhares@gmail.com>
2022-04-24 19:45:22 -0300

    Update to use Task.async_stream
    If we have millions of the user we can use the advantage of the lazy enumerables from Elixir Streams.
    
    Streams in Elixir contains the initial inputs and a list of functions to perform.
    
    This way we getting concurrency and performance without sacrificing reliability

--- sender/lib/sender.ex
+++ sender/lib/sender.ex
@@ -1,17 +1,20 @@
 defmodule Sender do
+  @max_concurrency 2
+
   def send_email(email) do
     Process.sleep(300)
     IO.puts("Email sent to #{email}")
     {:ok, "email_sent"}
   end
 
+  # Ther order slows the process down because async_stream will wait for
+  # a slow process to complete before moving on the next.
+  # For this task, we don't care about the order, so the ordered is set to false.
+
+  # We can kill the process by sending a kill signal using the option: on_timeout: :kill_task.
   def notify_all(emails) do
     emails
-    |> Enum.map(fn email ->
-      Task.async(fn ->
-        send_email(email)
-      end)
-    end)
-    |> Enum.map(&Task.await/1)
+    |> Task.async_stream(&send_email/1, max_concurrency: @max_concurrency, ordered: false)
+    |> Enum.to_list()
   end
 end
